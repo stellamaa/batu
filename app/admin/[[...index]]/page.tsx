748a778524a97f728be4c2d871adf0246d54d1d3
stellamaa <stellaam@live.com>
2026-03-17 16:17:46 +0000

    push

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -1,13 +1,9 @@
-import { redirect } from "next/navigation";
+'use client'
 
-export const dynamicParams = false;
-
-export function generateStaticParams() {
-  // Provide the base /admin path for static export.
-  return [{ index: [] as string[] }];
-}
+import React from 'react';
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../sanity.congif'
 
 export default function AdminPage() {
-  // GitHub Pages uses static export; Sanity Studio requires a server.
-  redirect("/");
+  return <NextStudio config={config} />;
 }
